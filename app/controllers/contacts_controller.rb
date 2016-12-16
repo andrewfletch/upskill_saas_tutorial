34d0f786100f564fe59c9e9c015648dd6b79ef44
Andrew Fletcher <andrewsfletch@gmail.com>
2016-12-16 17:28:03 +0000

    Added form validations to model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,18 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
+    @contact = Contact.new #creates new blank object stored in instance variable
   end
   
-  def create
-    @contact = Contact.new(contact_params)
+  def create #tell rails to save something
+    @contact = Contact.new(contact_params) #takes the form input values and assigns them to the new object (mass assignment)
     if @contact.save
       redirect_to new_contact_path, notice: "Message sent."
     else
-      redirect_tp new_contact_path, notice: "Error occured."
+      redirect_to new_contact_path, notice: "Error occured."
     end
   end
   
-  private
+  private #can only be used inside this file
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
